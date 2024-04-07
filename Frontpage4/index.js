266fc16eb42a7535800cce760ef7d097d955f99c
Neto Cruz <galaxy.net0710@gmail.com>
2024-04-07 16:28:01 -0600

    proyect front 4

---
+++ Frontpage4/index.js
@@ -0,0 +1,17 @@
+function dropactived(a){
+// document.getElementById('myDropdown').classList.toggle('show')
+a.parentNode.getElementsByClassName("dropdown")[0].classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbutton')) {
+      var dropdowns = document.getElementsByClassName("dropdown");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
